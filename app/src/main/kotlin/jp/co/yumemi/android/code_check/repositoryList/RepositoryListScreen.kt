f03a7b110dafb5debcfdbb1f0c68a3c6d9c0e1e5
itsukicode <t.itsuki928@gmail.com>
2022-12-14 19:02:48 +0900

    feat: RepositoryListScreenの大枠を作成

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repositoryList/RepositoryListScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repositoryList/RepositoryListScreen.kt
@@ -2,15 +2,33 @@ package jp.co.yumemi.android.code_check.repositoryList
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepositoryListScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Android Engineer CodeCheck") },
+            )
+        },
     ) {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
 
-
+        }
     }
+}
+
+@Preview
+@Composable
+fun RepositoryListScreenPreview() {
+    RepositoryListScreen()
 }
